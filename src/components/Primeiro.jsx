ba3eb32f98ba611e37b6e492e13bafa778f4f0cb
Fabricio Linhares Rodrigues <fabriciorodrigues1231@outlook.com>
2021-01-11 17:13:49 -0300

    primeiros componentes

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () =>
+    <>
+        <h1>Primeiro Componente (Arrow)</h1>
+        <p>div criada apartir de um componente</p>
+    </>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Primeiro from './components/Primeiro'
+import ComParametro from './components/ComParametro'
+
 ReactDOM.render(
-    <h1>Olá Mundo</h1>,
+    <div>
+        <Primeiro></Primeiro>
+        <ComParametro titulo="Título aqui"
+            subtitulo="Algo aqui" />
+    </div>
+    ,
     document.getElementById('root')
 )
