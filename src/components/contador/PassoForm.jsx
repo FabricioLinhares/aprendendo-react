08e06ef996c800a504be5ee6d7a7593e95e29289
Fabricio Linhares Rodrigues <fabriciorodrigues1231@outlook.com>
2021-01-12 19:59:31 -0300

    fix PassoForm

--- src/components/contador/PassoForm.jsx
+++ src/components/contador/PassoForm.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PassoForm = props => {
     return (
         <div>
-            <label for="passoInput">Passo: </label>
+            <label htmlFor="passoInput">Passo: </label>
             <input id="passoInput" type="number"
                 value={props.passo}
                 onChange={e => props.onPassoChange(+e.target.value)}/>
