e0e24a4a391f031e52e517dc540957bd7dca4ce3
Fabricio Linhares Rodrigues <fabriciorodrigues1231@outlook.com>
2021-01-12 19:46:51 -0300

    Contador

--- src/App.jsx
+++ src/App.jsx
@@ -18,60 +18,69 @@ import Super from './components/comunicacao/indireta/Super'
 
 import Input from './components/form/Input'
 
+import Contador from './components/contador/Contador'
 
 const App = () =>
     <div className="App">
-        <Card titulo="Primeiro Componente" cor="#85C4B9">
-            <Primeiro />
-        </Card>
-        <Card titulo="Componente com Parâmetro" cor="#F2E394">
-            <ComParametro titulo="Título aqui"
-                subtitulo="Algo aqui" />
-        </Card>
-        <Card titulo="Componente com Filhos" cor="#F2AE72">
-            <ComFilhos>
-                <ul>
-                    <li>Ana</li>
-                    <li>Maria</li>
-                    <li>Daniela</li>
-                    <li>Rodrigo</li>
-                    <li>Carlos</li>
-                </ul>
-            </ComFilhos>
-        </Card>
-        <Card titulo="Repetição" cor="#E8B71A">
-            <Repeticao />
-        </Card>
-        <Card titulo="Condicional" cor="#1FDA9A">
-            <Condicional numero={9} />
-        </Card>
-        <Card titulo="Condicional com if" cor="#5A6A62">
-            <CondicionalIf numero={10} />
-        </Card>
-        <Card titulo="Maiusculo" cor="#FA6900">
-            <Maiusculo frase="Algo escrito aqui" />
-        </Card>
-        <Card titulo="Comunicação direta" cor="#F2AE72">
-            <Pai sobrenome="Freitas"></Pai>
-        </Card>
-        <Card titulo="Comunicação indireta">
-            <Super></Super>
-        </Card>
-        <Card titulo="Input" cor="#D96459">
-            <Input></Input>
-        </Card>
+        <h1>Fundamentos React</h1>
 
-        {/* <Card titulo="Maiusculo com filhos">
-            <MaiusculoFilhos>
-                <div>
-                    <p>Frase</p>
-                    <span>algo</span>
-                </div>
-                <div>
-                    <p>Frase 2</p>
-                </div>
-            </MaiusculoFilhos>
-        </Card> */}
+        <div className="Cards">
+            <Card titulo="Primeiro Componente" cor="#85C4B9">
+                <Primeiro />
+            </Card>
+            <Card titulo="Componente com Parâmetro" cor="#F2E394">
+                <ComParametro titulo="Título aqui"
+                    subtitulo="Algo aqui" />
+            </Card>
+            <Card titulo="Componente com Filhos" cor="#F2AE72">
+                <ComFilhos>
+                    <ul>
+                        <li>Ana</li>
+                        <li>Maria</li>
+                        <li>Daniela</li>
+                        <li>Rodrigo</li>
+                        <li>Carlos</li>
+                    </ul>
+                </ComFilhos>
+            </Card>
+            <Card titulo="Repetição" cor="#E8B71A">
+                <Repeticao />
+            </Card>
+            <Card titulo="Condicional" cor="#1FDA9A">
+                <Condicional numero={9} />
+            </Card>
+            <Card titulo="Condicional com if" cor="#5A6A62">
+                <CondicionalIf numero={10} />
+            </Card>
+            <Card titulo="Maiusculo" cor="#FA6900">
+                <Maiusculo frase="Algo escrito aqui" />
+            </Card>
+            <Card titulo="Comunicação direta" cor="#F2AE72">
+                <Pai sobrenome="Freitas"></Pai>
+            </Card>
+            <Card titulo="Comunicação indireta">
+                <Super></Super>
+            </Card>
+            <Card titulo="Input" cor="#D96459">
+                <Input></Input>
+            </Card>
+            <Card titulo="Contador" cor="#008BBA">
+                <Contador passo={10} valor={100}></Contador>
+            </Card>
+
+            {/* <Card titulo="Maiusculo com filhos">
+                <MaiusculoFilhos>
+                    <div>
+                        <p>Frase</p>
+                        <span>algo</span>
+                    </div>
+                    <div>
+                        <p>Frase 2</p>
+                    </div>
+                </MaiusculoFilhos>
+            </Card> */}
+        </div>
+        
     </div>
 
 export default App
